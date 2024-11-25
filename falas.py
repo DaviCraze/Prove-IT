e69f4b082966ecb6aa4d8ed85c6845cf3d055158
DaviCraze <101135600+DaviCraze@users.noreply.github.com>
2024-11-25 18:34:25 -0300

    Update falas.py

--- falas.py
+++ falas.py
@@ -30,6 +30,10 @@ class Falas:
    
     def acabou(self):
          return self.index >= len(self.falas)
+    
+    @property
+    def mostrar_index(self):
+        return self.index
 
 cutscene_falas = {
     "boas_vindasL": Falas("boas_vindasL", [
@@ -53,11 +57,34 @@ cutscene_falas = {
         {"texto": "AQUI EMBAIXO SE ENCONTRA AS RESPOSTAS DA SUA PERGUNTA,\nCADA PORTA IRA CONTER UMA RESPOSTA, SENDO FALSA\nOU VERDADEIRA. ANDE ATÉ A PORTA E CLIQUE ENTER\nPARA SELECIONAR ELA.", "posicao": "topo"}
     ], bloqueia_jogo=True),
     "erro1": Falas("erro1", [
+        {"texto": "bleh", "posicao": "topo"},
         {"texto": "AAAH NÃO, VOCÊ ERROU A RESPOSTA!", "posicao": "topo"},
         {"texto": "MAS TUDO BEM, NÃO É COMO SE FOSSE O FIM DO MUNDO.\nVOCÊ AINDA TEM MUITAS CHANCES PARA MOSTRAR SEU\nBRILHO!!!", "posicao": "topo"},
         {"texto": "COMO VOCÊ PODE VER, NO CANTO DA TELA TEMOS\nA QUANTIDADE DE VEZES NA QUAL VOCÊ PODE\nERRAR UMA QUESTÃO", "posicao": "topo"},
         {"texto": "A COR REPRESENTA A QUANTIDADE DE VIDAS QUE VOCÊ TEM.\nAMARELO SENDO 3 VIDAS, LARANJA 2 VIDAS\nE VERMELHO 1 VIDA", "posicao": "topo"},
         {"texto": "ENTÃO VAMOS LA! TENTE DE NOVO, EU SEI QUE VOCÊ\nCONSEGUE!", "posicao": "topo"}
+    ], bloqueia_jogo=True),
+    "acerto": Falas("acerto", [
+        {"texto": "pu", "posicao": "topo"},
+        {"texto": "PARABENS!!!! VOCÊ ACERTOU A QUESTÃO! ISSO FOI\nINCRIVEL! ESTOU TAO ORGULHOSA T_T", "posicao": "topo"},
+        {"texto": "DESCULPA, ME EMOCIONEI MESMO EU SENDO UM ROBO\nQUE NÃO CONSEGUE DERRAMAR LAGRIMAS DE EMOÇÃO.", "posicao": "topo"},
+        {"texto": "VOLTANDO PRO PROMPT....", "posicao": "topo"},
+        {"texto": "VOCÊ CONSEGUIU ACERTAR A QUESTÃO! AGORA VOCÊ\nGANHOU PONTOS QUE DEMONSTRA SEU DESEMPENHO\nE SUA HONRA NA EMPRESA!", "posicao": "topo"},
+        {"texto": "SEUS PONTOS FICAM ACUMULADOS ALI NO CANTO DA TELA.\nCADA FORMATO DE QUESTÃO IRA TE DAR UMA QUANTIDADE\nDE PONTOS DIFERENTES.", "posicao": "baixo"},
+        {"texto": "VAMOS VER COM QUANTOS PONTOS VOCÊ VAI ESTAR\nNO FINAL DA SUA JORNADA!", "posicao": "baixo"},
+        {"texto": "ALIAS, UMA INFORMAÇÃO IMPORTANTE, VOCÊ TERÁ QUE\nRESPONDER QUESTÕES ATÉ A SALA 100, SIM, ISSO\nMESMO, SALA Nº100...", "posicao": "topo"},
+        {"texto": "EU SEI QUE PARECE UMA LONGA JORNADA, MAS TENHO\nCERTEZA QUE VOCÊ IRA CHEGAR ATÉ O FIM!", "posicao": "topo"}
+    ], bloqueia_jogo=True),
+    "saladesafio": Falas("saladesafio", [
+        {"texto": "miau", "posicao": "topo"},
+        {"texto": "VOCÊ CONSEGUIU CHEGAR NA SALA 10!!! VOCÊ EVOLUIU\nTÃO RAPIDO...QUE ORGULHO T_T", "posicao": "topo"},
+        {"texto": "DESCULPA... PROMETO NAO ME EMOCIONAR DE NOVO(EU ACHO)", "posicao": "topo"},
+        {"texto": "VOLTANDO AO PROMPT...", "posicao": "topo"},
+        {"texto": "VOCÊ CHEGOU NA SALA DESAFIO!! UMA SALA NA QUAL CONTEM\nUMA QUESTÃO DESAFIO, QUE SERA MAIS DIFICIL QUE AS OUTRAS\nQUE VOCÊ JA RESPONDEU...", "posicao": "topo"},
+        {"texto": "EM CADA SALA 10,20,30...ATÉ A SALA 100, ESSAS SALAS\nDESAFIO IRÃO APARECER PARA DIFICULTAR SUA CONCLUSAO.\nOU MELHOR DIZENDO, TE ENTREGAR MAIS INTELIGENCIA\nE INFORMAÇÕES", "posicao": "topo"},
+        {"texto": "NESSAS SALAS VOCÊ TERÁ UMA QUESTÃO LOGICA DE MATEMATICA\nNA QUAL VOCÊ TERÁ APENAS QUE RESPONDER SIM OU NÃO\nPARA A QUESTÃO, BEM SIMPLES NÉ?", "posicao": "baixo"},
+        {"texto": "CASO VOCÊ ACERTE A QUESTÂO, VOCÊ IRA GANHAR 35 PONTOS!", "posicao": "topo"},
+        {"texto": "SÓ ESCOLHER A PORTA COM A RESPOSTA QUE VOCÊ ACHA SER\nA CERTA!", "posicao": "topo"}
     ], bloqueia_jogo=True)
 }
                     
